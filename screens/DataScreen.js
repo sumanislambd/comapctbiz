d3898f738c57ea79314b465900f11ac1299762ab
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-12-05 22:06:03 -0500

    TabNav in Appjs and Screens boilerplate

---
+++ screens/DataScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class DataScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> DataScreen </Text>
+        <Text> DataScreen </Text>
+        <Text> DataScreen</Text>
+        <Text> DataScreen </Text>
+        <Text> DataScreen </Text>
+        <Text> DataScreen </Text>
+        <Text> DataScreen </Text>
+      </View>
+      );
+    }
+}
+
+export default DataScreen;
